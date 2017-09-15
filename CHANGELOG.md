18a22ebc4c32af869254ceeb8d3b75856bab2e4e
msimonin <matthieu.simonin@inria.fr>
2017-09-15 14:15:11 +0200

    inc version number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
-# 0.0.1 (current)
+# 0.0.2 (current)
+
+* TODO
+
+# 0.0.1
 
 * Initial version
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='enoslib',
-    version='0.0.1',
+    version='0.0.2',
     description='',
     url='https://github.com/beyondtheclouds/enoslib',
     author='msimonin',
