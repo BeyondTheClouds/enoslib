04437822a255285498ade3a669d8ee4e2d2d587d
msimonin <matthieu.simonin@inria.fr>
2017-10-03 16:54:17 +0200

    add chameleon/baremetal provider

---
+++ tests/functionnal/test_chameleonbaremetal.py
@@ -0,0 +1,39 @@
+from enoslib.api import generate_inventory, emulate_network, validate_network, wait_ssh
+from enoslib.infra.enos_chameleonbaremetal.provider import Chameleonbaremetal
+
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+provider_conf = {
+    "key_name": "enos-matt",
+    "resources": {
+        "machines": [{
+            "role": "control",
+            "flavor": "storage",
+            "number": 1,
+        },{
+            "role": "compute",
+            "flavor": "compute",
+            "number": 1,
+        }],
+        "networks": ["network_interface"]
+    }
+}
+
+tc = {
+    "enable": True,
+    "default_delay": "20ms",
+    "default_rate": "1gbit",
+}
+inventory = os.path.join(os.getcwd(), "hosts")
+provider = Chameleonbaremetal(provider_conf)
+provider.destroy()
+roles, networks = provider.init()
+generate_inventory(roles, networks, inventory)
+wait_ssh(inventory)
+generate_inventory(roles, networks, inventory, check_networks=True)
+emulate_network(roles, inventory, tc)
+validate_network(roles, inventory)
+provider.destroy()
