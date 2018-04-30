4cfb6f8c2f17db6174338551a128ca9bd9bdbf65
msimonin <matthieu.simonin@inria.fr>
2018-04-30 12:09:55 +0200

    inc version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="enoslib",
-    version="1.2.1",
+    version="1.2.2",
     description="",
     url="https://github.com/beyondtheclouds/enoslib",
     author="msimonin",
