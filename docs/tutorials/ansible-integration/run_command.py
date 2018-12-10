cad3b568e9f698f1a2d3876e3e8a3c631e4d294e
msimonin <matthieu.simonin@inria.fr>
2018-12-10 12:19:08 +0100

    [docs] ansible integration update
    with the new configuration object

--- docs/tutorials/ansible-integration/run_command.py
+++ docs/tutorials/ansible-integration/run_command.py
@@ -1,5 +1,6 @@
 from enoslib.api import generate_inventory, run_command
 from enoslib.infra.enos_vagrant.provider import Enos_vagrant
+from enoslib.infra.enos_vagrant.configuration import Configuration
 
 import json
 import os
@@ -11,21 +12,21 @@ logging.basicConfig(level=logging.DEBUG)
 provider_conf = {
     "resources": {
         "machines": [{
-            "role": "control1",
+            "roles":  ["control1"],
             "flavor": "tiny",
             "number": 1,
-            "networks": ["n1", "n2"]
         },{
-            "role": "control2",
+            "roles": ["control2"],
             "flavor": "tiny",
             "number": 1,
-            "networks": ["n1", "n3"]
-        }]
+        }],
+        "networks": [{"roles": ["rn1"], "cidr": "172.16.0.1/16"}]
     }
 }
 
 inventory = os.path.join(os.getcwd(), "hosts")
-provider = Enos_vagrant(provider_conf)
+conf = Configuration.from_dictionnary(provider_conf)
+provider = Enos_vagrant(conf)
 roles, networks = provider.init()
 generate_inventory(roles, networks, inventory, check_networks=True)
 result = run_command("control*", "date", inventory)
