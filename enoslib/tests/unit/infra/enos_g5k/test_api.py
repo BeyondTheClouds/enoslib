4682e8717b8eb6591602f075b63f27d4902f5ac5
msimonin <matthieu.simonin@inria.fr>
2019-04-01 16:52:42 +0200

    fix tests

--- enoslib/tests/unit/infra/enos_g5k/test_api.py
+++ enoslib/tests/unit/infra/enos_g5k/test_api.py
@@ -37,6 +37,7 @@ class TestDeploy(EnosTest):
 
     @mock.patch("enoslib.infra.enos_g5k.api._get_grid5000_client")
     def test_prod(self, mock_gk):
+        site = "rennes"
         nodes = ["foocluster-1", "foocluster-2"]
         r = Resources({
             "resources":{
@@ -44,44 +45,48 @@ class TestDeploy(EnosTest):
                     "_c_nodes": nodes,
                     "primary_network": "network1"
                 }],
-                "networks": [{"type": PROD, "id": "network1"}]
+                "networks": [{"type": PROD, "id": "network1", "site": site}]
             }
         })
         deployed = set(["foocluster-1", "foocluster-2"])
         undeployed = set()
-        utils._deploy = mock.Mock(return_value=(deployed, undeployed))
+        r.driver.deploy = mock.Mock(return_value=(deployed, undeployed))
         r.deploy()
-        utils._deploy.assert_called_with(nodes, False, {"env_name": DEFAULT_ENV_NAME})
+        r.driver.deploy.assert_called_with(site, nodes, False, {"env_name": DEFAULT_ENV_NAME})
         self.assertCountEqual(deployed, r.c_resources["machines"][0]["_c_deployed"])
         self.assertCountEqual(undeployed, r.c_resources["machines"][0]["_c_undeployed"])
 
     @mock.patch("enoslib.infra.enos_g5k.api._get_grid5000_client")
     def test_vlan(self, mock_gk):
+        site = "rennes"
         nodes = ["foocluster-1", "foocluster-2"]
-        _c_network = utils.ConcreteVlan(vlan_id="4", site="rennes")
+        _c_network = utils.ConcreteVlan(vlan_id="4", site=site)
         r = Resources({
             "resources": {
                 "machines": [{
                     "_c_nodes": nodes,
                     "primary_network": "network1"
                 }],
-                "networks": [{"type": KAVLAN, "id": "network1", "_c_network": _c_network}]
+                "networks": [{"type": KAVLAN, "id": "network1", "_c_network": _c_network, "site": "rennes"}]
             }
         })
         deployed = set(["foocluster-1", "foocluster-2"])
         undeployed = set()
-        utils._deploy = mock.Mock(return_value=(deployed, undeployed))
+
+        r.driver.deploy = mock.Mock(return_value=(deployed, undeployed))
         r.deploy()
-        utils._deploy.assert_called_with(nodes, False, {"env_name": DEFAULT_ENV_NAME, "vlan": "4"})
+        r.driver.deploy.assert_called_with(site, nodes, False, {"env_name": DEFAULT_ENV_NAME, "vlan": "4"})
+        self.assertCountEqual(deployed, r.c_resources["machines"][0]["_c_deployed"])
         self.assertCountEqual(deployed, r.c_resources["machines"][0]["_c_deployed"])
         self.assertCountEqual(undeployed, r.c_resources["machines"][0]["_c_undeployed"])
 
     @mock.patch("enoslib.infra.enos_g5k.api._get_grid5000_client")
     def test_2_deployements_with_undeployed(self, mock_gk):
+        site = "rennes"
         nodes_foo = ["foocluster-1", "foocluster-2"]
         nodes_bar = ["barcluster-1", "barcluster-2"]
 
-        _c_network_1 = utils.ConcreteProd(site="rennes")
+        _c_network_1 = utils.ConcreteProd(site=site)
         _c_network_2 = utils.ConcreteVlan(vlan_id="4", site="rennes")
         r = Resources({
             "resources": {
@@ -93,20 +98,19 @@ class TestDeploy(EnosTest):
                     "primary_network": "network2"
                 }],
                 "networks": [
-                    {"type": PROD, "id": "network1", "_c_network": _c_network_1},
-                    {"type": KAVLAN, "id": "network2", "_c_network": _c_network_2}]
+                    {"type": PROD, "id": "network1", "_c_network": _c_network_1, "site": "rennes"},
+                    {"type": KAVLAN, "id": "network2", "_c_network": _c_network_2, "site": "rennes"}]
             }
         })
         d_foo = set(["foocluster-1"])
         u_foo = set(nodes_foo) - d_foo
         d_bar = set(["barcluster-2"])
         u_bar = set(nodes_bar) - d_bar
-        utils._deploy = mock.Mock(side_effect=[(d_foo, u_foo), (d_bar, u_bar)])
+
+        r.driver.deploy = mock.Mock(side_effect=[(d_foo, u_foo), (d_bar, u_bar)])
         r.deploy()
-        self.assertEqual(2, utils._deploy.call_count)
+        self.assertEqual(2, r.driver.deploy.call_count)
         self.assertCountEqual(d_foo, r.c_resources["machines"][0]["_c_deployed"])
         self.assertCountEqual(u_foo, r.c_resources["machines"][0]["_c_undeployed"])
         self.assertCountEqual(d_bar, r.c_resources["machines"][1]["_c_deployed"])
         self.assertCountEqual(u_bar, r.c_resources["machines"][1]["_c_undeployed"])
-
-
