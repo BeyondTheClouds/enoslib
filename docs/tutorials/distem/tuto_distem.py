db220e725905d80333dc99ef68481e1ad25b805c
msimonin <matthieu.simonin@inria.fr>
2019-08-27 14:50:52 +0200

    cosmetic + distem as a dependency

--- docs/tutorials/distem/tuto_distem.py
+++ docs/tutorials/distem/tuto_distem.py
@@ -16,20 +16,20 @@ inventory = os.path.join(os.getcwd(), "hosts")
 # claim the resources
 conf = Configuration.from_settings(job_name="wip-distem",
                                    force_deploy=FORCE,
-                                   image="file:///home/rolivo/public/distem-fs-jessie.tar.gz")\
-    .add_machine(roles=["compute"],
-                 cluster="parapide",
-                 number=50,
-                 flavour="tiny")\
-    .add_machine(roles=["controller"],
-                 cluster="parapluie",
-                 number=1,
-                 flavour="tiny")\
-    .finalize()
+                                   image="file:///home/msimonin/public/distem-fs-jessie.tar.gz")\
+                    .add_machine(roles=["controller"],
+                                 cluster="parapluie",
+                                 number=1,
+                                 flavour="large")\
+                    .add_machine(roles=["compute"],
+                                 cluster="parapluie",
+                                 number=50,
+                                 flavour="medium")\
+                    .finalize()
+
 provider = Distem(conf)
 
 roles, networks = provider.init()
-
 print(roles)
 print(networks)
 
