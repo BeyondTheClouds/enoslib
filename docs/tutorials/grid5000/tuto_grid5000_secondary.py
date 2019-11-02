f8beddeda73f98793aa2ce618fac1b3f207db4d9
msimonin <matthieu.simonin@inria.fr>
2019-11-02 11:08:19 +0100

    Doc/g5k: secondary interfaces

---
+++ docs/tutorials/grid5000/tuto_grid5000_secondary.py
@@ -0,0 +1,49 @@
+from enoslib.infra.enos_g5k.provider import G5k
+from enoslib.infra.enos_g5k.configuration import Configuration, NetworkConfiguration
+
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+SITE = "rennes"
+CLUSTER = "paravance"
+
+network = NetworkConfiguration(
+    id="n1",
+    type="prod",
+    roles=["my_network"],
+    site=SITE
+)
+private = NetworkConfiguration(
+    id="n2",
+    type="kavlan",
+    roles=["private"],
+    site=SITE
+)
+
+conf = (
+    Configuration()
+    .add_network_conf(network)
+    .add_network_conf(private)
+    .add_machine(
+        roles=["server"],
+        cluster=CLUSTER,
+        nodes=1,
+        primary_network=network,
+        secondary_networks=[private]
+    )
+    .add_machine(
+        roles=["client"],
+        cluster=CLUSTER,
+        nodes=1,
+        primary_network=network,
+        secondary_networks=[private]
+    )
+    .finalize()
+)
+
+provider = G5k(conf)
+roles, networks = provider.init()
+provider.destroy()
