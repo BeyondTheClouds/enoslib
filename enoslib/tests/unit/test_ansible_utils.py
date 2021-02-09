0c5421a1e18d841162919ba126e0677565c824e2
msimonin <matthieu.simonin@inria.fr>
2021-02-09 22:44:24 +0100

    unit test for Host.filter_addresses

--- enoslib/tests/unit/test_ansible_utils.py
+++ enoslib/tests/unit/test_ansible_utils.py
@@ -234,6 +234,17 @@ class TestEqHosts(EnosTest):
             "(we do not look at `extra` for SSH)",
         )
 
+        h1 = TestEqHosts._make_host()
+        h2 = TestEqHosts._make_host()
+        h2.extra_devices = set([NetDevice(name="eth0")])
+        self.assertEqual(
+            h1.__hash__(),
+            h2.__hash__(),
+            "Hosts have the same hash because we can "
+            "SSH on each of them in the same manner "
+            "(we do not look at `extra_devices` for SSH)",
+        )
+
     def test__extra(self):
         extra1 = {"extra_ip": "5.6.7.8"}
         extra2 = {"extra_ip": "5.6.7.8"}
@@ -252,3 +263,91 @@ class TestEqHosts(EnosTest):
         extra1.update(extra_ip="1.2.3.4")
         h2 = TestEqHosts._make_host(extra1)
         self.assertNotEqual(h1, h2, "Extra is not shared across Hosts")
+
+
+class TestFilterAddresses(EnosTest):
+    def test_filter_addresses(self):
+        h1 = Host("1.2.3.4")
+        self.assertCountEqual(
+            [], h1.filter_addresses(), "No device attached means" "No address to get"
+        )
+
+        h1 = Host("1.2.3.4")
+        h1.extra_devices = set([NetDevice(name="eth0", addresses=set())])
+        self.assertCountEqual(
+            [],
+            h1.filter_addresses(),
+            "One device attached but no address" "No address to get",
+        )
+
+        h1 = Host("1.2.3.4")
+        address = IPAddress("1.2.3.4", None)
+        h1.extra_devices = set([NetDevice(name="eth0", addresses=set([address]))])
+        self.assertCountEqual(
+            [],
+            h1.filter_addresses(),
+            "One device attached with one address but no network attached"
+            "No address to get",
+        )
+        self.assertCountEqual(
+            [address],
+            h1.filter_addresses(include_unknown=True),
+            "One device attached with one address but no network attached"
+            "One address to get if include_unknown=True",
+        )
+
+        h1 = Host("1.2.3.4")
+        address = IPAddress(
+            "1.2.3.4", DefaultNetwork(roles=["a"], address="1.2.3.0/24")
+        )
+        h1.extra_devices = set([NetDevice(name="eth0", addresses=set([address]))])
+        self.assertCountEqual(
+            [address],
+            h1.filter_addresses(),
+            "One device attached with one address and a known network"
+            "One address to get",
+        )
+
+        h1 = Host("1.2.3.4")
+        network = DefaultNetwork(roles=["a"], address="1.2.3.0/24")
+        address = IPAddress(
+            "1.2.3.4", network
+        )
+        h1.extra_devices = set([NetDevice(name="eth0", addresses=set([address]))])
+        self.assertCountEqual(
+            [address],
+            h1.filter_addresses([network]),
+            "One device attached with one address and a known network"
+            "One address to get (not filtered out)",
+        )
+
+        h1 = Host("1.2.3.4")
+        network = DefaultNetwork(roles=["a"], address="1.2.3.0/24")
+        network2 = DefaultNetwork(roles=["a"], address="1.2.4.0/24")
+        address = IPAddress(
+            "1.2.3.4", network
+        )
+        h1.extra_devices = set([NetDevice(name="eth0", addresses=set([address]))])
+        self.assertCountEqual(
+            [],
+            h1.filter_addresses([network2]),
+            "One device attached with one address and a known network"
+            "No address to get (filtered out)",
+        )
+
+        h1 = Host("1.2.3.4")
+        network = DefaultNetwork(roles=["a"], address="1.2.3.0/24")
+        network2 = DefaultNetwork(roles=["a"], address="1.2.4.0/24")
+        address = IPAddress(
+            "1.2.3.4", network
+        )
+        address2 = IPAddress(
+            "1.2.4.4", network2
+        )
+        h1.extra_devices = set([NetDevice(name="eth0", addresses=set([address, address2]))])
+        self.assertCountEqual(
+            [address],
+            h1.filter_addresses([network]),
+            "One device attached with two addresses on two known network"
+            "One address to get (one filtered out)",
+        )
