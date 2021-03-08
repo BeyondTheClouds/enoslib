8201bf0e2682ed7f489f3fa9f2eee2be26dae8e3
msimonin <matthieu.simonin@inria.fr>
2021-03-08 14:06:59 +0100

    🏴

--- docs/tutorials/grid5000/tuto_grid5000_non_deploy_docker.py
+++ docs/tutorials/grid5000/tuto_grid5000_non_deploy_docker.py
@@ -15,20 +15,14 @@ network = en.G5kNetworkConf(id="n1", type="prod", roles=["my_network"], site=SIT
 conf = (
     en.G5kConf.from_settings(job_type="allow_classic_ssh", job_name=job_name)
     .add_network_conf(network)
-    .add_machine(
-        roles=["control"], cluster=CLUSTER, nodes=2, primary_network=network
-    )
+    .add_machine(roles=["control"], cluster=CLUSTER, nodes=2, primary_network=network)
     .finalize()
 )
 
 provider = en.G5k(conf)
 roles, networks = provider.init()
 
-registry_opts = dict(
-    type="external",
-    ip=f"docker-mirror.{SITE}.grid5000.fr",
-    port=80
-)
+registry_opts = dict(type="external", ip=f"docker-mirror.{SITE}.grid5000.fr", port=80)
 
 d = en.Docker(agent=roles["control"], registry_opts=registry_opts)
 d.deploy()
