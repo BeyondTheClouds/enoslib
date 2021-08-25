675ea2cafa2a3e1b5b964ca70630cd378761a2fd
msimonin <matthieu.simonin@inria.fr>
2021-08-25 17:06:48 +0200

    remove unused functions

--- enoslib/utils.py
+++ enoslib/utils.py
@@ -7,27 +7,6 @@ import os
 from enoslib.errors import EnosFilePathError
 
 
-def get_roles_as_list(desc):
-    # NOTE(msimonin): role and roles are mutually exclusive in theory We'll fix
-    # the schemas later in the mean time to not break user code let's remove
-    # duplicates here
-    roles = desc.get("roles", [])
-    if roles:
-        return roles
-
-    role = desc.get("role", [])
-    if role:
-        roles = [role]
-
-    return roles
-
-
-def gen_rsc(roles):
-    for _, hosts in roles.items():
-        for host in hosts:
-            yield host
-
-
 def _check_tmpdir(tmpdir):
     if not os.path.exists(tmpdir):
         os.mkdir(tmpdir)
