83c622e7d5e039463cb964ce4806bf1b330c4c2a
msimonin <matthieu.simonin@inria.fr>
2022-01-17 22:10:58 +0100

    vmong5k: debian10 -> debian11

--- enoslib/infra/enos_vmong5k/constants.py
+++ enoslib/infra/enos_vmong5k/constants.py
@@ -7,7 +7,7 @@ DEFAULT_JOB_NAME = "EnOslib-vmong5k"
 DEFAULT_QUEUE = "default"
 DEFAULT_WALLTIME = "02:00:00"
 # See  https://intranet.grid5000.fr/bugzilla/show_bug.cgi?id=10855
-DEFAULT_IMAGE = "/grid5000/virt-images/debian10-x64-nfs.qcow2"
+DEFAULT_IMAGE = "/grid5000/virt-images/debian11-x64-nfs.qcow2"
 PROVIDER_PATH = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
 PLAYBOOK_PATH = os.path.join(PROVIDER_PATH, "ansible", "site.yml")
