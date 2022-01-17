1839caf8d41ab0267f4e884cc8734e9e56fd17b5
msimonin <matthieu.simonin@inria.fr>
2022-01-17 13:16:28 +0100

    vmong5k: fix target type for cdrom

--- docs/tutorials/vmong5k/tuto_vmong5k.py
+++ docs/tutorials/vmong5k/tuto_vmong5k.py
@@ -1,28 +1,29 @@
 import logging
 from pathlib import Path
 
-from enoslib import *
+import enoslib as en
 
-logging.basicConfig(level=logging.DEBUG)
+
+logging.basicConfig(level=logging.INFO)
 
 job_name = Path(__file__).name
 
 # claim the resources
 conf = (
-    VMonG5kConf
+    en.VMonG5kConf
     .from_settings(job_name=job_name, gateway=True)
     .add_machine(
         roles=["docker", "compute"],
-        cluster="parapluie",
-        number=2,
+        cluster="paravance",
+        number=3,
         flavour_desc={
-            "core": 4,
-            "mem": 4096
+            "core": 1,
+            "mem": 1024
         }
     )
     .add_machine(
         roles=["docker", "controller"],
-        cluster="parapluie",
+        cluster="paravance",
         number=3,
         flavour="tiny"
     )
@@ -30,22 +31,22 @@ conf = (
 )
 
 
-provider = VMonG5k(conf)
+provider = en.VMonG5k(conf)
 
 roles, networks = provider.init()
 print(roles)
 print(networks)
 
-wait_for(roles)
+en.wait_for(roles)
 # install docker on the nodes
 # bind /var/lib/docker to /tmp/docker to gain some places
-docker = Docker(agent=roles["docker"], bind_var_docker="/tmp/docker")
+docker = en.Docker(agent=roles["docker"], bind_var_docker="/tmp/docker")
 docker.deploy()
 
 # start containers.
 # Here on all nodes
-with play_on(pattern_hosts="*", roles=roles) as p:
-    p.docker_container(
+with en.actions(pattern_hosts="*", roles=roles) as a:
+    a.docker_container(
         name="mycontainer",
         image="nginx",
         ports=["80:80"],
