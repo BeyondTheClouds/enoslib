f398a622886b602a1edd5087d4bfd0d025a13eb6
Matthieu Simonin <matthieu.simonin@inria.fr>
2022-09-05 12:31:20 +0200

    docs: multi_providers add idempotency

--- docs/tutorials/multi_providers/tuto_fit_edge_to_g5k_cloud.py
+++ docs/tutorials/multi_providers/tuto_fit_edge_to_g5k_cloud.py
@@ -48,7 +48,7 @@ iotlab_conf = {
                 "number": 1,
             }
         ]
-    }
+    },
 }
 iotlab_conf = en.IotlabConf.from_dictionnary(iotlab_conf)
 iotlab = en.Iotlab(iotlab_conf)
@@ -59,39 +59,36 @@ roles, networks = providers.init()
 g5k, iotlab = providers.providers
 
 # Firewall rules
-g5k.fw_create(
-    hosts=roles["server"],
-    port=[1883],
-    src_addr=None,
-    proto="tcp+udp"
-)
+with g5k.firewall(hosts=roles["server"], port=[1883], src_addr=None, proto="tcp+udp"):
+    en.run("dhclient -6 br0", roles=roles["server"])
+    roles = en.sync_info(roles, networks)
 
-en.run("dhclient -6 br0", roles=roles["server"])
-roles = en.sync_info(roles, networks)
+    cloud_server = roles["server"][0]
+    addresses = cloud_server.filter_addresses(networks=networks["my_network"])
+    # get only the ipv6 address
+    floating_ip = [str(a.ip.ip) for a in addresses if isinstance(a.ip, IPv6Interface)][
+        0
+    ]
 
-cloud_server = roles["server"][0]
-addresses = cloud_server.filter_addresses(networks=networks["my_network"])
-# get only the ipv6 address
-floating_ip = [str(a.ip.ip) for a in addresses if isinstance(a.ip, IPv6Interface)][0]
+    logging.info(f"Cloud server IP: {floating_ip}")  # 2001:660:4406:700:1::28
 
-logging.info(f"Cloud server IP: {floating_ip}")  # 2001:660:4406:700:1::28
+    # Experimentation logic starts here
+    # G5K Cloud
+    dest_dir = "/tmp"
+    with en.play_on(roles=roles["server"]) as p:
+        p.copy(src="./artifacts_cloud/", dest=dest_dir)
+        p.shell(f"cd {dest_dir} && bash {dest_dir}/cloud_worker.sh > {dest_dir}/tests")
+    # FIT IoT LAB
+    with en.play_on(roles=roles["client"]) as p:
+        p.copy(src="./artifacts_edge", dest=dest_dir)
+        p.shell(
+            f"bash {dest_dir}/artifacts_edge/edge_worker.sh edge_data 100 {floating_ip}"
+        )
 
-
-# Experimentation logic starts here
-# G5K Cloud
-dest_dir = "/tmp"
-with en.play_on(roles=roles["server"]) as p:
-    p.copy(src="./artifacts_cloud/", dest=dest_dir)
-    p.shell(f"cd {dest_dir} && bash {dest_dir}/cloud_worker.sh > {dest_dir}/tests")
-# FIT IoT LAB
-with en.play_on(roles=roles["client"]) as p:
-    p.copy(src="./artifacts_edge", dest=dest_dir)
-    p.shell(f"bash {dest_dir}/artifacts_edge/edge_worker.sh edge_data 100 {floating_ip}")
-
-# How to check execution?
-# firewall rule: https://api.grid5000.fr/stable/sites/rennes/firewall/1909896
-# ssh to the Cloud server: ssh root@paravance-40.rennes.grid5000.fr
-# root@paravance-40:~# tail -f /tmp/predict.log
-# you may also check mosquitto topic (mosquitto_sub_img.py downloads images received in running dir):
-# (venv) drosendo@frennes:~$ python mosquitto_sub_img.py --topic edge_data --mqtt_broker 2001:660:4406:700:1::28
-# (venv) drosendo@grenoble:~$ python mosquitto_sub_img.py --topic edge_data --mqtt_broker 2001:660:4406:700:1::28
+    # How to check execution?
+    # firewall rule: https://api.grid5000.fr/stable/sites/rennes/firewall/1909896
+    # ssh to the Cloud server: ssh root@paravance-40.rennes.grid5000.fr
+    # root@paravance-40:~# tail -f /tmp/predict.log
+    # you may also check mosquitto topic (mosquitto_sub_img.py downloads images received in running dir):
+    # (venv) drosendo@frennes:~$ python mosquitto_sub_img.py --topic edge_data --mqtt_broker 2001:660:4406:700:1::28
+    # (venv) drosendo@grenoble:~$ python mosquitto_sub_img.py --topic edge_data --mqtt_broker 2001:660:4406:700:1::28
