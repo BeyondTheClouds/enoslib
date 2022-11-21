05a0c19ac7ca1a0b61b2d553c4c48bef0e41e05d
Matthieu Simonin <matthieu.simonin@inria.fr>
2022-11-21 23:42:39 +0100

    doc: update to debian11(vagrant)

--- docs/tutorials/ansible-integration/flent_on.py
+++ docs/tutorials/ansible-integration/flent_on.py
@@ -25,8 +25,6 @@ roles, networks = provider.init()
 
 # Experimentation logic starts here
 with en.actions(roles=roles) as p:
-    # flent requires python3, so we default python to python3
-    p.shell("update-alternatives --install /usr/bin/python python /usr/bin/python3 1")
     p.apt_repository(
         repo="deb http://deb.debian.org/debian stretch main contrib non-free",
         state="present",
