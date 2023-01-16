77038def530a95782705012757940cdcdaff8f6c
Alexis BITAILLOU <alexis.bitaillou@inria.fr>
2023-01-16 20:30:02 +0000

    Adding typing on enos_chameleonkvm package

--- enoslib/infra/enos_chameleonkvm/provider.py
+++ enoslib/infra/enos_chameleonkvm/provider.py
@@ -1,11 +1,16 @@
+from typing import Optional, Tuple
+
 import enoslib.infra.enos_openstack.provider as openstack
+from enoslib.objects import Roles, Networks
 
 
 class Chameleonkvm(openstack.Openstack):
-    def init(self, force_deploy=False, **kwargs):
+    def init(
+        self, force_deploy: bool = False, start_time: Optional[int] = None, **kwargs
+    ) -> Tuple[Roles, Networks]:
         return super().init(force_deploy)
 
-    def destroy(self, wait=False):
+    def destroy(self, wait: bool = False, **kwargs):
         super().destroy()
 
     def offset_walltime(self, offset: int):
