5f76129144bb740e5025cfd470776b29620bc3ca
Alexis BITAILLOU <alexis.bitaillou@inria.fr>
2023-01-24 10:19:44 +0000

    CI on G5K

---
+++ docs/tutorials/cleanup_g5k_job.py
@@ -0,0 +1,12 @@
+import logging
+import sys
+import enoslib as en
+
+
+en.init_logging(level=logging.DEBUG)
+
+job_name = sys.argv[1]
+conf = en.G5kConf.from_settings(job_name=job_name)
+provider = en.G5k(conf)
+provider.destroy()
+exit(1)
