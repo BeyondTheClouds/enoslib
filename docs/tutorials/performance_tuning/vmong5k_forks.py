d971b77386610dbd91f68b262c0ea7984e9b20d6
Baptiste Jonglez <baptiste.jonglez@inria.fr>
2023-08-03 13:38:17 +0200

    VMonG5K: Document vcore_type and placement strategies

--- docs/tutorials/performance_tuning/vmong5k_forks.py
+++ docs/tutorials/performance_tuning/vmong5k_forks.py
@@ -1,4 +1,8 @@
+# Make sure your run this example from a dedicated Grid'5000 control node.
+# See https://discovery.gitlabpages.inria.fr/enoslib/tutorials/performance_tuning.html
+
 import logging
+import os
 from pathlib import Path
 
 import enoslib as en
@@ -9,12 +13,16 @@ en.check()
 # Set very high parallelism to be able to handle a large number of VMs
 en.set_config(ansible_forks=100)
 
+# Enable Ansible pipelining
+os.environ["ANSIBLE_PIPELINING"] = "True"
+
 job_name = Path(__file__).name
 
-conf = en.VMonG5kConf.from_settings(job_name=job_name, walltime="0:45:00").add_machine(
+conf = en.VMonG5kConf.from_settings(job_name=job_name, walltime="00:45:00").add_machine(
     roles=["fog"],
     cluster="dahu",
     number=200,
+    vcore_type="thread",
     flavour_desc={"core": 1, "mem": 2048},
 )
 
