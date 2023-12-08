336e863c856d6dc51bb15a816e625a7ebe9bee47
Baptiste Jonglez <baptiste.jonglez@inria.fr>
2023-12-08 14:31:30 +0100

    CI: update g5k tutorial used in CI, ecotype no longer has a second NIC

--- docs/tutorials/grid5000/tuto_grid5000_reload.py
+++ docs/tutorials/grid5000/tuto_grid5000_reload.py
@@ -8,7 +8,7 @@ en.check()
 
 job_name = Path(__file__).name
 
-nantes_net = en.G5kNetworkConf(type="kavlan", roles=["nantes"], site="nantes")
+nancy_net = en.G5kNetworkConf(type="kavlan", roles=["nancy"], site="nancy")
 rennes_net = en.G5kNetworkConf(type="kavlan", roles=["rennes"], site="rennes")
 
 conf = (
@@ -18,10 +18,10 @@ conf = (
         job_name=job_name,
         walltime="0:30:00",
     )
-    .add_network_conf(nantes_net)
+    .add_network_conf(nancy_net)
     .add_network_conf(rennes_net)
     .add_machine(
-        roles=["nantes"], cluster="ecotype", nodes=1, secondary_networks=[nantes_net]
+        roles=["nancy"], cluster="gros", nodes=1, secondary_networks=[nancy_net]
     )
     .add_machine(
         roles=["rennes"], cluster="paravance", nodes=1, secondary_networks=[rennes_net]
