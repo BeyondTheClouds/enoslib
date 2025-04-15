5bce4e6c494b4cfef8d6dc22532ea9babbc8802c
Rajiv Mayani <mayani@isi.edu>
2025-04-15 14:23:59 +0200

    feat(chameleon): Add support for multiple machines with different images in...

--- enoslib/tests/unit/infra/enos_chameleonbaremetal/test_configuration.py
+++ enoslib/tests/unit/infra/enos_chameleonbaremetal/test_configuration.py
@@ -35,3 +35,114 @@ class TestConfiguration(EnosTest):
         conf.finalize()
         self.assertEqual(1, len(conf.machines))
         self.assertEqual(1, len(conf.networks))
+
+    def test_only_global_image(self):
+        image = "CC-Ubuntu22.04"
+        d = {
+            "walltime": "01:00:00",
+            "lease_name": "lease-name",
+            "rc_file": "rc_file",
+            "key_name": "key-name",
+            "image": image,
+            "resources": {
+                "machines": [
+                    {
+                        "roles": ["worker"],
+                        "flavour": "compute_zen3",
+                        "number": 1,
+                    }
+                ],
+                "networks": ["sharednet1"],
+            },
+        }
+        conf = Configuration.from_dictionary(d)
+
+        self.assertEqual(image, conf.image)
+        self.assertEqual(image, conf.machines[0].image)
+
+    def test_no_global__no_local_image(self):
+        d = {
+            "walltime": "01:00:00",
+            "lease_name": "lease-name",
+            "rc_file": "rc_file",
+            "key_name": "key-name",
+            "resources": {
+                "machines": [
+                    {
+                        "roles": ["worker"],
+                        "flavour": "compute_zen3",
+                        "number": 1,
+                    }
+                ],
+                "networks": ["sharednet1"],
+            },
+        }
+        conf = Configuration.from_dictionary(d)
+        conf.finalize()
+
+        self.assertEqual(constants.DEFAULT_IMAGE, conf.image)
+        self.assertEqual(constants.DEFAULT_IMAGE, conf.machines[0].image)
+
+    def test_no_global_different_local_images(self):
+        image = "CC-Ubuntu22.04"
+        d = {
+            "walltime": "01:00:00",
+            "lease_name": "lease-name",
+            "rc_file": "rc_file",
+            "key_name": "key-name",
+            "resources": {
+                "machines": [
+                    {
+                        "roles": ["worker"],
+                        "flavour": "compute_zen3",
+                        "number": 1,
+                        "image": image,
+                    },
+                    {
+                        "roles": ["worker"],
+                        "flavour": "compute_zen3",
+                        "number": 1,
+                        "image": image.replace("22", "24"),
+                    },
+                ],
+                "networks": ["sharednet1"],
+            },
+        }
+        conf = Configuration.from_dictionary(d)
+        conf.finalize()
+
+        self.assertEqual(constants.DEFAULT_IMAGE, conf.image)
+        self.assertEqual(image, conf.machines[0].image)
+        self.assertEqual(image.replace("22", "24"), conf.machines[1].image)
+
+    def test_global_image(self):
+        image = "CC-Ubuntu22.04"
+        d = {
+            "walltime": "01:00:00",
+            "lease_name": "lease-name",
+            "rc_file": "rc_file",
+            "key_name": "key-name",
+            "image": image.replace("22", "24"),
+            "resources": {
+                "machines": [
+                    {
+                        "roles": ["worker"],
+                        "flavour": "compute_zen3",
+                        "number": 1,
+                        "image": image,
+                    },
+                    {
+                        "roles": ["worker"],
+                        "flavour": "compute_zen3",
+                        "number": 1,
+                    },
+                ],
+                "networks": ["sharednet1"],
+            },
+        }
+        conf = Configuration.from_dictionary(d)
+        conf.finalize()
+
+        self.assertEqual(image.replace("22", "24"), conf.image)
+        self.assertEqual(image, conf.machines[0].image)
+        self.assertEqual(image.replace("22", "24"), conf.machines[1].image)
